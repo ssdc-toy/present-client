188d86c3c9d671ccdbe578e42a9b04b6e7d5e917
sodam <damsoshin@gmail.com>
2022-12-20 17:21:37 +0900

    🔧 절대경로 설정

---
+++ components/layout.jsx
@@ -0,0 +1,4 @@
+const Layout = ({ children }) => {
+  return <div class="mx-auto max-w-sm px-4 py-16 bg-slate-100">{children}</div>;
+};
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ export default function Home() {
       <Head>
         <title>Present</title>
       </Head>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
     </>
   );
 }
